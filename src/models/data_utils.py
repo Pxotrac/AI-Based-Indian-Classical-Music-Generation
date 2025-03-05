a997464e68643cff9ade2971c1e457a9c1810707
Pxotrac <91304521+Pxotrac@users.noreply.github.com>
2025-03-05 11:25:46 +0530

    Updated

--- src/models/data_utils.py
+++ src/models/data_utils.py
@@ -200,25 +200,22 @@ def tokenize_sequence(tokenizer, sequence):
     tokenized_sequence = tokenizer.texts_to_sequences([sequence])  # Pass as a list of strings
     return tokenized_sequence[0]  # Return the first (and only) list of tokenized sequences
 
+
 def create_sequences(tokenized_notes, sequence_length, batch_size, raag_labels):
     """Creates sequences from tokenized notes, batching and adding raag labels."""
     logging.info("Creating sequences...")
     sequences_with_labels = []
-    overall_note_index = 0  # Add an index to keep track of the overall position in raag_labels
-    for seq in tokenized_notes: #changed
+    for i, seq in enumerate(tokenized_notes): #changed
         if len(seq) < sequence_length + 1:  # Add +1 to account for the target note
-          overall_note_index += len(seq) #added
           continue
         for j in range(0, len(seq) - sequence_length):
             input_sequence = seq[j:j+sequence_length]
-            
-            if overall_note_index+j < len(raag_labels): # check if the index is within the labels
-              target_raag_id = raag_labels[overall_note_index+j]  # Get raag_id from raag_labels
+            # Check if raag_labels has enough labels
+            if len(raag_labels) > j: # changed
+              target_raag_id = raag_labels[j]  # Get raag_id from raag_labels
               sequences_with_labels.append((input_sequence, target_raag_id, seq[j+sequence_length]))
             else:
-              logging.warning(f"Not enough labels for index {overall_note_index+j}. Skipping.")
-
-        overall_note_index += len(seq) # added
+              logging.warning(f"Not enough labels for sequence {j}. Skipping.") #changed j for i
 
     # Shuffle and batch the data
     np.random.shuffle(sequences_with_labels)
