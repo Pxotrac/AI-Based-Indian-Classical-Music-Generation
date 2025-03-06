2f215a5b2f1ca27b50f13ecc3d1693cb934db970
Pxotrac <91304521+Pxotrac@users.noreply.github.com>
2025-03-06 23:22:02 +0530

    Refactor training script to enhance logging, streamline data preprocessing, and add model training visualization; implement early stopping and model checkpointing.

--- src/train.py
+++ src/train.py
@@ -4,32 +4,19 @@ import time
 import yaml
 import pickle
 import tensorflow as tf
-from models.data_utils import load_and_preprocess_data, extract_all_notes, create_sequences, create_raag_id_mapping, generate_raag_labels, tokenize_all_notes, create_tokenizer
+import matplotlib.pyplot as plt
+from models.data_utils import load_and_preprocess_data, extract_all_notes, create_tokenizer, create_sequences, create_raag_id_mapping, generate_raag_labels, tokenize_all_notes
 from models.model_builder import create_model
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
-from tqdm import tqdm
 
 # Set up logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s',
-                    handlers=[logging.FileHandler("train.log"), logging.StreamHandler()])
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
+# Use the default strategy for CPU and single GPU
+strategy = tf.distribute.get_strategy()
+print("REPLICAS: ", strategy.num_replicas_in_sync)
 
 def main():
-    logging.info("Starting train process...")
-    start_time_total = time.time()
-
-    # Check for GPU
-    if tf.config.list_physical_devices('GPU'):
-        logging.info("GPU is available and being used in train.py.")
-        print("GPU is available and being used in train.py.")
-    else:
-        logging.warning("No GPU detected. Running on CPU in train.py.")
-        print("No GPU detected. Running on CPU in train.py.")
-
-    # Use the default strategy for CPU and single GPU
-    strategy = tf.distribute.get_strategy()
-    logging.info(f"REPLICAS in train.py: {strategy.num_replicas_in_sync}")
-
     if os.environ.get("COLAB_GPU", "FALSE") == "TRUE":
         repo_dir = "/content/drive/MyDrive/music_generation_repo"
         data_path = "/content/drive/MyDrive/"  # correct path
@@ -59,95 +46,138 @@ def main():
     start_time = time.time()
 
     # Load and preprocess data once
-    all_output, selected_raags, _ = load_and_preprocess_data(repo_dir, data_path)  # we dont need unique_raags
+    all_output = load_and_preprocess_data(repo_dir, data_path)
+    logging.info("Data loaded.")
     if all_output is None or len(all_output) == 0:
         logging.error("No data was loaded. Check the data. Aborting")
         return
 
     # Extract all notes
-    all_notes, all_output_filtered = extract_all_notes(all_output, selected_raags)
-
+    all_notes, all_output_filtered = extract_all_notes(all_output)
     if len(all_notes) == 0:
         logging.error("No notes extracted. Check data loading and preprocessing. Aborting.")
         return
 
-    logging.info(f"Data preprocessing took {time.time() - start_time:.2f} seconds")
-
-    # Tokenizer Creation
-    logging.info("Creating Tokenizer...")
-    start_time_tokenizer = time.time()
+    # Tokenization and vocabulary creation
     tokenizer = create_tokenizer(all_notes)
-    tokenizer_path = os.path.join(repo_dir, "tokenizers", f"{tokenizer_name}.pickle")
-    with open(tokenizer_path, 'wb') as f:
-        pickle.dump(tokenizer, f)
-    logging.info(f"Tokenizer saved to {tokenizer_path}")
-    logging.info(f"Tokenizer creation took {time.time() - start_time_tokenizer:.2f} seconds")
-
     if tokenizer is None:
         logging.error("Tokenizer was not created. Check preprocessing. Aborting")
         return
+
     if len(tokenizer.word_index) < 2:
         logging.error("Tokenizer created a vocabulary with less than 2 words. Check your notes data. Aborting.")
         return
-
     vocab_size = len(tokenizer.word_index) + 1
     logging.info(f"Vocab size: {vocab_size}")
+
     if vocab_size <= 1:
         logging.error(f"The vocabulary size is too small: {vocab_size}. Please check your data.")
         return
 
     # Raag ID mapping
     logging.info("Creating raag ID mapping...")
-    start_time_mapping = time.time()
-    raag_id_dict, num_raags = create_raag_id_mapping(all_output_filtered, selected_raags)
-    logging.info(f"Raag ID mapping complete. total: {num_raags}")
-    logging.info(f"Raag ID mapping took {time.time() - start_time_mapping:.2f} seconds")
-
+    raag_id_dict, num_raags = create_raag_id_mapping(all_output_filtered)
+    logging.info("Raag ID mapping complete")
+    logging.info(f"Number of raags: {num_raags}")
     if num_raags == 0:
         logging.error("No raags were found. Please check your data.")
         return
 
     # Generate raag labels
     logging.info("Generating raag labels...")
-    start_time_labels = time.time()
     raag_labels = generate_raag_labels(all_output_filtered, raag_id_dict, num_raags, all_notes, sequence_length)
     logging.info("Raag labels generated")
-    logging.info(f"Raag labels took {time.time() - start_time_labels:.2f} seconds")
 
     # Tokenize all notes
-    start_time_tokenize = time.time()
     tokenized_notes = tokenize_all_notes(tokenizer, all_notes)
-    logging.info(f"Tokenizing all notes took {time.time() - start_time_tokenize:.2f} seconds")
+    logging.debug(f"Number of tokenized_notes: {len(tokenized_notes)}")
+    if tokenized_notes:
+        if len(tokenized_notes)>=10:
+            logging.debug(f"First 10 tokenized_notes: {tokenized_notes[:10]}")
+        
+        logging.debug(f"First tokenized note: {tokenized_notes[0]}")
+    else:
+        logging.warning("tokenized_notes is empty.")
 
+    # Check sequence length and raag labels length
     logging.info(f"Sequence length: {sequence_length}")
     logging.info(f"Number of raag labels: {len(raag_labels)}")
-    logging.info(f"Number of tokenized_notes: {len(tokenized_notes)}")
+    logging.info(f"Batch size: {batch_size * strategy.num_replicas_in_sync}")
 
-    # Create sequences
+    logging.info(f"Dataset size before creation: {len(tokenized_notes) - sequence_length}")
+    # Create sequences using tf.data.Dataset
     sequences_dataset = create_sequences(tokenized_notes, sequence_length, batch_size * strategy.num_replicas_in_sync, raag_labels)
+    logging.info("Data preprocessing complete.")
 
-    # Model Creation and training
+    end_time = time.time()
+    logging.info(f"Data preprocessing took {end_time - start_time:.2f} seconds")
+
+    # Split the dataset in train and validation
+    logging.info(f"Dataset size: {tf.data.experimental.cardinality(sequences_dataset).numpy()}")
+    dataset_size = tf.data.experimental.cardinality(sequences_dataset).numpy()
+    train_size = int(dataset_size * (1 - validation_split))
+    train_dataset = sequences_dataset.take(train_size)
+    validation_dataset = sequences_dataset.skip(train_size)
+
+    # Model Creation, compilation and training within strategy.scope()
     with strategy.scope():
         model = create_model(vocab_size, num_raags, sequence_length, strategy)
+        model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+        # Model Checkpoint
+        checkpoint_filepath = os.path.join(repo_dir, "checkpoints", f"{model_name}.h5")
+        model_checkpoint_callback = ModelCheckpoint(
+            filepath=checkpoint_filepath,
+            save_weights_only=True,
+            monitor='val_loss',
+            mode='min',
+            save_best_only=True)
+
+        # Early Stopping
+        early_stopping = EarlyStopping(monitor='val_loss', patience=patience, restore_best_weights=True)
+
+        # Training
+        logging.info("Starting model training...")
+        training_start_time = time.time()
+        history = model.fit(
+            train_dataset,
+            validation_data=validation_dataset,
+            epochs=epochs,
+            callbacks=[early_stopping, model_checkpoint_callback],
+            verbose=1
+        )
+        training_end_time = time.time()
+        logging.info(f"Model training took {training_end_time - training_start_time:.2f} seconds")
+
+        # Save the model
         model_path = os.path.join(repo_dir, "models", f"{model_name}.h5")
-        # Check if the model file exists before loading
-        # Callbacks
-        early_stopping = EarlyStopping(monitor='loss', patience=patience)
-        model_checkpoint = ModelCheckpoint(filepath=model_path,
-                                           monitor='loss',
-                                           save_best_only=True,
-                                           save_weights_only=True)
+        model.save(model_path)
+        logging.info(f"Model saved to {model_path}")
 
-        # Train the model
-        logging.info("Starting training...")
-        start_time_training = time.time()
-        model.fit(sequences_dataset, epochs=epochs, callbacks=[early_stopping, model_checkpoint])
-        logging.info("Training finished.")
-        logging.info(f"Training took {time.time() - start_time_training:.2f} seconds")
+        # Save tokenizer
+        tokenizer_path = os.path.join(repo_dir, "tokenizers", f"{tokenizer_name}.pickle")
+        with open(tokenizer_path, 'wb') as f:
+            pickle.dump(tokenizer, f)
+        logging.info(f"Tokenizer saved to {tokenizer_path}")
 
-    logging.info(f"Total train execution time: {time.time() - start_time_total:.2f} seconds")
-    logging.info("Train process completed.")
+        # Plot training history
+        plt.figure(figsize=(12, 6))
+        plt.subplot(1, 2, 1)
+        plt.plot(history.history['accuracy'], label='Training Accuracy')
+        plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+        plt.legend()
+        plt.title('Training and Validation Accuracy')
+
+        plt.subplot(1, 2, 2)
+        plt.plot(history.history['loss'], label='Training Loss')
+        plt.plot(history.history['val_loss'], label='Validation Loss')
+        plt.legend()
+        plt.title('Training and Validation Loss')
+
+        plt.tight_layout()
+        os.makedirs(os.path.join(repo_dir, "plots"), exist_ok=True)
+        plt.savefig(os.path.join(repo_dir, "plots", f"{model_name}_training_history.png"))
+        logging.info("Training history plot saved.")
 
 if __name__ == "__main__":
     main()
-
