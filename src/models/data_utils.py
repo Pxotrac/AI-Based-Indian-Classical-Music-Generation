1c809d528eb142c6487f38979f0cda909720666f
Pxotrac <91304521+Pxotrac@users.noreply.github.com>
2025-03-07 03:28:20 +0530

    Refactor create_sequences function to simplify window splitting and improve raag label handling

--- src/models/data_utils.py
+++ src/models/data_utils.py
@@ -244,13 +244,12 @@ def create_sequences(tokenized_notes, sequence_length, batch_size, raag_labels):
     dataset = tf.data.Dataset.from_tensor_slices((tokenized_notes_tensor,raag_labels_tensor ))
     dataset = dataset.window(size=sequence_length + 1, shift=1, drop_remainder=True)
     
-    def split_window(window_notes, window_raag):
-      """Split the window into features and label."""
-      notes_seq = window_notes[:-1]
-      target_note = window_notes[-1]
-      raag_label = window_raag[0:sequence_length] #use the sequence length
-
-      return {"notes_input": notes_seq, "raag_label": raag_label}, target_note
+    def split_window(notes_window, raag_window):
+        """Split the window into features and label."""
+        notes_seq = notes_window[:-1]
+        target_note = notes_window[-1]
+        raag_label = raag_window #use the sequence length
+        return {"notes_input": notes_seq, "raag_label": raag_label}, target_note
     
     def flat_window(window_notes, window_raag):
         """Flatten windows to create sequences."""
